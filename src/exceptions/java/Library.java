01eeea5a140e3b259b73078e70a1058e6cd4030e
FabrizioMisseri <misserifabrizio@gmail.com>
2023-03-14 15:54:12 +0100

    add compiler in Library

--- src/exceptions/java/Library.java
+++ src/exceptions/java/Library.java
@@ -2,18 +2,46 @@ package exceptions.java;
 
 import java.util.Scanner;
 
+//        String title;
+//        int pageNumber;
+//        String author;
+//        String publisher;
+
 public class Library {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        try {
-            Book ciao = new Book("dsdfgdsg",-2,"dasfasdf","dsafasdfas");
-        } catch (Exception e) {
-            e.getMessage();
-        } finally {
-            Book ciao = new Book("",0,"","");
-            ciao.toString();
-            System.out.println("FINE ");
+        System.out.println("Benvenuto in SASSONEshop.com, qunati libri vuoi inserire?");
+
+        int booksLength = Integer.parseInt(scan.nextLine());
+        Book[] books = new Book[booksLength];
+        int i = 0;
+
+            while (i < booksLength) {
+                try {
+                    System.out.println("inserisci titolo:");
+                    String title = scan.nextLine();
+                    System.out.println("inserisci numero pagine:");
+                    int pageNumber = Integer.parseInt(scan.nextLine());
+                    System.out.println("inserisci autore:");
+                    String author = scan.nextLine();
+                    System.out.println("inserisci titolo:");
+                    String publisher = scan.nextLine();
+                    Book book = new Book(title, pageNumber, author, publisher);
+                    books[i] = book;
+                    i++;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+
+        for (int j = 0; j < books.length; j++) {
+            System.out.println(books[j].toString());
+            System.out.println();
         }
+
+        scan.close();
 
     }
 
